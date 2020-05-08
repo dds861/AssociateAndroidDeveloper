692d780a5c733748aeb4ba8facf7dac2a3c4a16e
Daniyar <dds861@gmail.com>
2020-05-09 00:02:23 +0200

    Android Developers -> Create a notification

--- app/src/main/java/com/dd/associateandroiddeveloper/MainActivity.kt
+++ app/src/main/java/com/dd/associateandroiddeveloper/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.dd.associateandroiddeveloper
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -34,6 +39,41 @@ class MainActivity : AppCompatActivity() {
         val mySnackbar = Snackbar.make(btnSnackbar, R.string.app_name, Snackbar.LENGTH_SHORT)
         mySnackbar.setAction("Undo", SnackbarAad())
         btnSnackbar.setOnClickListener { mySnackbar.show() }
+
+
+        /**
+         * Notifications
+         */
+        btnNotification.setOnClickListener {
+
+            // Create PendingIntent
+            val resultIntent = Intent(this, MainActivity::class.java)
+            val resultPendingIntent = PendingIntent.getActivity(
+                this, 0, resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+
+            // Create Notification
+            val builder = NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Title")
+                .setContentText("Notification text")
+                .setContentIntent(resultPendingIntent)
+
+            val notification = builder.build()
+
+            // Show Notification
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(1, notification)
+        }
+
+        btnCancelNotification.setOnClickListener {
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.cancel(1)
+        }
+
 
     }
 }
